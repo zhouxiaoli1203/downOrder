9a8eb839c8ce03d082e37239a57ae4f0c4bf50c1
zmz_zhu <675446219@qq.com>
2021-04-21 17:03:19 +0800

    order

---
+++ src/components/header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      下单次数剩余：1000次
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'header',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
